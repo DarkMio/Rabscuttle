babc3d15dff765c5b087692b87e4fd5b7ef234be
DarkMio <s59330@beuth-hochschule.de>
2016-07-20 14:21:57 +0200

    Basic Input/Ouput works actually pretty fine.

--- Rabscuttle/core/MainEntry.cs
+++ Rabscuttle/core/MainEntry.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Rabscuttle.networking;
 
 namespace Rabscuttle {
     public class MainEntry {
         static void Main(string[] args) {
-            Debug.Print("Hello world.");
+          
+            Client ss = new Client("localhost", 6667);
+            ReceiveEverything(ss);
+            ss.Send("Real Name", null, "USER sessionId irc.foobar.net irc.foobar.net", null);
+            Thread.Sleep(500);
+            ReceiveEverything(ss);
+            ss.Send(null, null, "NICK UncreativePersonName", null);
+            Thread.Sleep(500);
+            NetworkMessage msg = ss.Receive();
+            Debug.WriteLine(msg);
+            ss.Send(msg.message, null, "PONG", null);
+            Thread.Sleep(500);
+            ReceiveEverything(ss);
+            ss.Send("A bunch of text.", null, "PING", null);
+            Thread.Sleep(500);
+            ReceiveEverything(ss);
+            Debug.WriteLine("Done sending");
+        }
+
+        static void ReceiveEverything(Client client) {
+            while (true) {
+                try {
+                    var response = client.Receive();
+                    if (response == null) {
+                        break;
+                    }
+                    Debug.WriteLine(response);
+                } catch (ArgumentException e) {
+                    Debug.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
