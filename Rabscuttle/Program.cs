f83b4ea58bba0a41fcf3a21b7553886fd8c3750f
DarkMio <s59330@beuth-hochschule.de>
2016-07-20 09:12:58 +0200

    Initial Commit?

--- Rabscuttle/Program.cs
+++ Rabscuttle/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace Rabscuttle {
     class Program {
         static void Main(string[] args) {
+            Debug.Print("Hello world.");
         }
     }
 }
