f6eeab07ba12ecf82d807300db1dc264e48260f4
DarkMio <s59330@beuth-hochschule.de>
2016-08-04 23:15:28 +0200

    Void Handler doing void things.

---
+++ Rabscuttle/handler/VoidHandler.cs
@@ -0,0 +1,15 @@
+﻿using Rabscuttle.networking.io;
+
+namespace Rabscuttle.handler {
+    /// <summary> Sole purpose to dump all network messages in it - everyone is free to subscribe </summary>
+    /// <seealso cref="Rabscuttle.handler.ObservableHandler" />
+    public class VoidHandler : ObservableHandler {
+        public override void HandleCommand(NetworkMessage message) {
+            NotifyAll(message);
+        }
+
+        public override void HandleReply(NetworkMessage message) {
+            NotifyAll(message);
+        }
+    }
+}
